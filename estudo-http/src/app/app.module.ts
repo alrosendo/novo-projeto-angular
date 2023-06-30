3504e653e43c4cd0ddfd5b102feaa3b5bd756238
alrosendo <alrosendo@gmail.com>
2023-06-30 04:26:20 -0300

    primeiro uploud de tarefas

---
+++ estudo-http/src/app/app.module.ts
@@ -0,0 +1,23 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { ListaTarefasComponent } from './components/lista-tarefas/lista-tarefas.component';
+import { AddTarefaComponent } from './componenets/add-tarefa/add-tarefa.component';
+import { EditarTarefaComponent } from './componets/editar-tarefa/editar-tarefa.component';
+import {HttpClientModule} from '@angular/common/http'
+import { ReactiveFormsModule } from '@angular/forms';
+@NgModule({
+  declarations: [
+    AppComponent,
+    ListaTarefasComponent,
+    AddTarefaComponent,
+    EditarTarefaComponent
+  ],
+  imports: [BrowserModule,AppRoutingModule,NgbModule, HttpClientModule, ReactiveFormsModule],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule {}
